9a526070baecf556a464cd38bc5751b4fad63cfe
Nevaan <pawel.losek@best.krakow.pl>
2015-11-11 22:57:33 +0100

    Amin func

---
+++ src/amin.erl
@@ -0,0 +1,15 @@
+-module(amin).
+-export([amin/1]).
+
+amin({[A|B]})->
+	amin({A,B});
+amin({Min,[]}) -> 
+	Min;
+amin({Min,[A|B]}) 
+	when Min < A ->
+		amin({Min,B});
+amin({Min,[A|B]}) ->
+	amin({A,B});
+amin({[]}) -> io:fwrite("Pusta lista").
+
+		
