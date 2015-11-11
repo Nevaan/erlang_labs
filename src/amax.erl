cdf8f5b2e73f75888d01c3ab90f97b7778bd0ed8
Nevaan <pawel.losek@best.krakow.pl>
2015-11-11 23:01:42 +0100

    Amax func

---
+++ src/amax.erl
@@ -0,0 +1,17 @@
+-module(amax).
+-export([amax/1]).
+
+amax({[A|B]}) ->
+	amax({A,B});
+amax({Max,[]}) ->
+	Max;
+amax({Max,[A|B]}) 
+	when Max < A ->
+		amax({A,B});
+amax({Max,[A|B]}) ->
+	amax({Max,B});
+amax({[]}) -> io:fwrite("Pusta lista").
+
+	
+	
+
